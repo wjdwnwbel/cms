b430c463c3c2e96ee52e7dcc1905ede918fb30f7
dd <wjdwnwbel@gmail.com>
2023-01-02 05:38:42 +0900

    feat : [003-EMAIL_VERIFICATION] 고객 이메일 verification

--- user-api/src/main/java/com/jjeong/cms/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/jjeong/cms/user/service/SignUpCustomerService.java
@@ -3,16 +3,57 @@ package com.jjeong.cms.user.service;
 import com.jjeong.cms.user.domain.SignUpForm;
 import com.jjeong.cms.user.domain.model.Customer;
 import com.jjeong.cms.user.domain.repository.CustomerRepository;
+import com.jjeong.cms.user.exception.CustomException;
+import com.jjeong.cms.user.exception.ErrorCode;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
+import javax.swing.text.html.Option;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpCustomerService {
-
 	private final CustomerRepository customerRepository;
 
 	public Customer signUp(SignUpForm form) {
 		return customerRepository.save(Customer.from(form));
+	}
+
+	public boolean isEmailExist(String email) {
+		return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
+			.isPresent();
+	}
+
+	// 이메일 인증
+	@Transactional
+	public void verifyEmail(String email, String code) {
+		Customer customer = customerRepository.findByEmail(email).orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_USER));
+		if(customer.isVerify()) {
+			throw new CustomException(ErrorCode.ALREADY_VERIFY);
+		} else if(!customer.getVerificationCode().equals(code)) {
+			throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+		} else if(customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+			throw new CustomException(ErrorCode.EXPIRE_CODE);
+		}
+
+		customer.setVerify(true);
+	}
+
+	// 인증이 완료된 고객 상태 변경
+	@Transactional
+	public LocalDateTime ChangesCustomerValidateEmail(Long customerId, String verificationCode) {
+		Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+		if(customerOptional.isPresent()) {
+			Customer customer = customerOptional.get();
+			customer.setVerificationCode(verificationCode);
+			customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+
+			return  customer.getVerifyExpiredAt();
+		}
+		throw new CustomException(ErrorCode.NOT_FOUND_USER);
 	}
 }
